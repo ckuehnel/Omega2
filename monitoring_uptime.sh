0597ea73c56cbbccd9029bc4908cde24ebd4aced
Claus Kühnel <info@ckuehnel.ch>
2017-02-06 17:55:33 +0100

    Add files via upload

--- monitoring_uptime.sh
+++ monitoring_uptime.sh
@@ -5,4 +5,5 @@ MSG=`cat UPT`
 echo -n Uptime:
 echo "$MSG"
 
-/home/OmegaPushover.sh "$MSG"
+curl --data "value1=$MSG" https://maker.ifttt.com/trigger/Uptime/with/key/dy0QeH-N3Ezfa4Ilst-r4F
+#curl -X POST -H "Content-Type: application/json" -d '{"value1":$msg}' https://maker.ifttt.com/trigger/Uptime/with/key/dy0QeH-N3Ezfa4Ilst-r4F
