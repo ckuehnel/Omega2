449f939166bce3e13feb05103bc191a51da501cb
Claus Kühnel <info@ckuehnel.ch>
2017-02-06 17:57:24 +0100

    Update monitoring_uptime.sh

--- monitoring_uptime.sh
+++ monitoring_uptime.sh
@@ -5,5 +5,5 @@ MSG=`cat UPT`
 echo -n Uptime:
 echo "$MSG"
 
+# Send data as Twitter direct message via IFTTT
 curl --data "value1=$MSG" https://maker.ifttt.com/trigger/Uptime/with/key/dy0QeH-N3Ezfa4Ilst-r4F
-#curl -X POST -H "Content-Type: application/json" -d '{"value1":$msg}' https://maker.ifttt.com/trigger/Uptime/with/key/dy0QeH-N3Ezfa4Ilst-r4F
